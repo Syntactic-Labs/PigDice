061da86df0f8e05af44f98609e2836831b6df135
Michael Walker <walkerrepair255@yahoo.com>
2021-10-20 14:08:41 -0400

    Add project files.

---
+++ PigDice/PigDice.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDice
+{
+    public class PigDice
+    {
+        public int RunToHighScore(int highScore)
+        {
+            var score = PlayPigDice();
+            var currHighScore = 0;
+            while (score < highScore)
+            {
+                score = PlayPigDice();
+                if (score > currHighScore)
+                {
+                    currHighScore = score;
+                    Console.WriteLine($"current highscore {score}");
+                }
+                
+            }
+            return score;
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        private Random rnd = new Random();
+        public int PlayPigDice()
+        {
+            var score = 0;
+            var roll = RollDice();
+            while(roll != 1)
+            {
+                score += roll;
+                roll = RollDice();
+            }
+            return score;
+        }
+        private int RollDice()
+        {
+            return rnd.Next(1, 7);
+
+        }
+
+    }
+}
---
+++ PigDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PigDice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var pd = new PigDice();
+            var score = pd.RunToHighScore(750);
+            var highest = 0;
+            if (score > highest)
+            {
+                highest = score;
+                
+            }
+
+            Console.WriteLine($"{score}");
+
+
+        }
+    }
+}
